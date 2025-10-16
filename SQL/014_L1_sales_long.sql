e778cf9511e236a0b4c2000e79afa14d04f22356
Jonny Greenwood <jonnylgreenwood@gmail.com>
2025-10-16 15:53:43 +0100

    Added sales long script - unpivot sales table

--- SQL/014_L1_sales_long.sql
+++ SQL/014_L1_sales_long.sql
@@ -1,11 +1,13 @@
-ATTACH 'SQL/L0_forecast_accuracy.duckdb' AS l0 (READ_ONLY);
-
 ------------------------------------------------------------
--- 1. Copy the sales table from L0 to L1
+-- Unpivot L1 sales_base into a long (fact) format
 ------------------------------------------------------------
-CREATE OR REPLACE TABLE l1_sales AS
+CREATE OR REPLACE TABLE l1_sales_long AS
 SELECT
+    product_id,
     store_id,
-    item_id AS product_id,
-    d_1:d_1913
-FROM l0.sales_train_validation;
+    day_col AS d,
+    NULLIF(sales_value, 0) AS sales  -- optional: replace 0s with NULL here
+FROM l1_sales
+UNPIVOT (sales_value FOR day_col IN (d_1:d_1913))
+WHERE sales IS NOT NULL;  -- optional: filter out rows where sales is NULL
+DESCRIBE l1_sales_long;
