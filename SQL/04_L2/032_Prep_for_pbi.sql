1bf52906b7b4444922a3d9d0bb6bfd7d00393571
Jonny Greenwood <jonnylgreenwood@gmail.com>
2025-10-17 23:30:11 +0100

    Added sales_profile to parque and updated DB path

--- SQL/04_L2/032_Prep_for_pbi.sql
+++ SQL/04_L2/032_Prep_for_pbi.sql
@@ -1,34 +1,20 @@
-COPY (SELECT p.product_key, st.store_key, l2.sales, c.date, s.sell_price,
-         s.sell_price * l2.sales AS revenue
-FROM l2_sales_long l2
-LEFT JOIN dim_calendar c
-    ON l2.d = c.d
-LEFT JOIN dim_sell_prices s
-    ON l2.product_id = s.product_id
-    AND l2.store_id = s.store_id
-    AND c.wm_yr_wk = s.week_id
-LEFT JOIN dim_product p
-    ON l2.product_id = p.product_id
-LEFT JOIN dim_store st
-    ON l2.store_id = st.store_id
-)
+ATTACH 'SQL/L2_forecast_accuracy.duckdb' AS l2;
+USE l2;
+
+COPY (SELECT * FROM l2.l2_sales_long)
   TO 'SQL/outputs/parquet/fact_sales_enriched.parquet' (FORMAT PARQUET);
 
-
-COPY (SELECT * FROM dim_calendar)
+COPY (SELECT * FROM l2.dim_calendar)
   TO 'SQL/outputs/parquet/dim_calendar.parquet' (FORMAT PARQUET);
 
-COPY (SELECT * FROM dim_event)
-  TO 'SQL/outputs/parquet/dim_event.parquet' (FORMAT PARQUET);
-
-COPY (SELECT * FROM dim_product)
+COPY (SELECT * FROM l2.dim_product)
   TO 'SQL/outputs/parquet/dim_product.parquet' (FORMAT PARQUET);
 
-COPY (SELECT * FROM dim_snap)
-  TO 'SQL/outputs/parquet/dim_snap.parquet' (FORMAT PARQUET);
-
-COPY (SELECT * FROM dim_sell_prices)
+COPY (SELECT * FROM l2.dim_sell_prices)
   TO 'SQL/outputs/parquet/dim_sell_prices.parquet' (FORMAT PARQUET);
 
-COPY (SELECT * FROM dim_store)
+COPY (SELECT * FROM l2.dim_store)
   TO 'SQL/outputs/parquet/dim_store.parquet' (FORMAT PARQUET);
+
+COPY (SELECT * FROM l2.sales_profile)
+  TO 'SQL/outputs/parquet/sales_profile.parquet' (FORMAT PARQUET);
