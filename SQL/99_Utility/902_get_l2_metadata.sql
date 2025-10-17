232911eed99368b73ef4d6a9758d15d3a752111b
Jonny Greenwood <jonnylgreenwood@gmail.com>
2025-10-17 12:02:32 +0100

    DQ for sales - havign issues with dropped data, need comparisons

---
+++ SQL/99_Utility/902_get_l2_metadata.sql
@@ -0,0 +1,26 @@
+-- Cleanly reattach all databases
+DETACH DATABASE IF EXISTS l0;
+DETACH DATABASE IF EXISTS l1;
+DETACH DATABASE IF EXISTS l2;
+
+ATTACH 'SQL/L1_forecast_accuracy.duckdb' AS l1 (READ_ONLY);
+
+-- Combine column metadata across all attached DBs
+CREATE OR REPLACE TABLE meta_all_columns AS
+SELECT
+  database_name,
+  schema_name,
+  table_name,
+  column_name,
+  data_type,
+  is_nullable,
+  column_index
+FROM duckdb_columns
+WHERE database_name IN ('l2')
+ORDER BY database_name, table_name, column_index;
+
+COPY (
+  SELECT *
+  FROM meta_all_columns
+) TO 'SQL/outputs/csv_for_markdown/l2_db_columns.csv' (FORMAT CSV, HEADER TRUE);
+
