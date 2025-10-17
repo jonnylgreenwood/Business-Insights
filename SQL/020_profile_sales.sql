7ca39134c79aa1ec2a55e6df2d3c9e969bd48eda
Jonny Greenwood <jonnylgreenwood@gmail.com>
2025-10-17 11:59:19 +0100

    moved to subfolders

--- SQL/020_profile_sales.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    COUNT(*) AS total_rows,
-    AVG(sales) AS avg_sales,
-    MIN(sales) AS min_sales,
-    MAX(sales) AS max_sales,
-    STDDEV(sales) AS std_sales,
-    APPROX_QUANTILE(sales, 0.5) AS median_sales
-FROM l1_sales_long;
