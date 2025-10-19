8be8a7c55d9f5f6bcf5c0b7b68def1d3d100adb1
Jonny Greenwood <jonnylgreenwood@gmail.com>
2025-10-19 12:36:01 +0100

    changed file path from having " " to "_"

--- SQL/03_Profile_Data/020_profile_sales.sql
+++ SQL/03_Profile_Data/020_profile_sales.sql
@@ -1,3 +1,6 @@
+ATTACH 'SQL/L1_forecast_accuracy.duckdb' AS l2;
+USE l2;
+
 SELECT
     COUNT(*) AS total_rows,
     AVG(sales) AS avg_sales,
@@ -5,4 +8,4 @@ SELECT
     MAX(sales) AS max_sales,
     STDDEV(sales) AS std_sales,
     APPROX_QUANTILE(sales, 0.5) AS median_sales
-FROM l1_sales_long;
+FROM l2_sales_long;
