a795e931d443ca31c8b3e35944aad80336370e58
Jonny Greenwood <jonnylgreenwood@gmail.com>
2025-10-22 00:28:41 +0100

    not used at all

--- SQL/04_L2/00_create_snap.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE TABLE dim_snap AS
-SELECT date, 'CA' AS state, snap_CA AS snap_flag
-FROM dim_calendar
-UNION ALL
-SELECT date, 'TX' AS state, snap_TX AS snap_flag
-FROM dim_calendar
-UNION ALL
-SELECT date, 'WI' AS state, snap_WI AS snap_flag
-FROM dim_calendar;
